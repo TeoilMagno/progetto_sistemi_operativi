e64db0663dba503c7ec9c6d0c62b06b5a8ec8284
TeoilMagno <matteomagnani2005@gmail.com>
2026-02-23 17:53:37 +0100

    preapata una base per phase2

---
+++ phase2/headers/kernel.c
@@ -0,0 +1,12 @@
+#ifndef KERNEL
+#define KERNEL
+
+#include "../../headers/listx.h"
+#include "../../headers/types.h"
+#include "../../phase1/headers/asl.h"
+#include "../../phase1/headers/pcb.h"
+#include <uriscv/liburiscv.h>
+
+int main();
+
+#endif
---
+++ phase2/kernel.c
@@ -0,0 +1,6 @@
+#include "./headers/kernel.c"
+
+int main()
+{
+
+}
