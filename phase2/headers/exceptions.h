8bba41b529b7be1bd6b0d60c75c3ea9b8974994c
TeoilMagno <matteomagnani2005@gmail.com>
2026-03-04 10:55:22 +0100

    inizializzazione exception handler

---
+++ phase2/headers/exceptions.h
@@ -0,0 +1,13 @@
+#ifndef EXCEPTION_HANDLER
+#define EXCEPTION_HANDLER
+
+#include "../../headers/listx.h"
+#include "../../headers/types.h"
+#include "../../phase1/headers/asl.h"
+#include "../../phase1/headers/pcb.h"
+#include "./initial.h"
+#include <uriscv/liburiscv.h>
+
+void exceptionHandler();
+
+#endif
