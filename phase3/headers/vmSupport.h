23f9334a71c6268a8b513685a79df563fea7a135
notALake <79274694+notALake@users.noreply.github.com>
2026-08-20 23:29:42 +0200

    Piccole correzioni pt 1
    Added sysSupport.h include and new function declarations.

--- phase3/headers/vmSupport.h
+++ phase3/headers/vmSupport.h
@@ -3,8 +3,8 @@
 
 #include "../../headers/types.h"
 #include "../../headers/klog.h"
-#include "./functions.h"
 #include "./const.h"
+#include "./sysSupport.h"
 #include <uriscv/cpu.h>
 #include <uriscv/liburiscv.h>
 #include <uriscv/arch.h>
@@ -12,8 +12,10 @@
 
 extern swap_t swap_pool[POOLSIZE];
 extern int swapPoolSemaphore;
-static int frameIndex;
 
+void initSwapStructs();
+int findPageIndex(unsigned int pte_entryHI);
+void writeToDevice(swap_t *frame);
 void readFromDevice(pteEntry_t *page, swap_t *frame, int p);
 void pager();
 int pageReplacement();
